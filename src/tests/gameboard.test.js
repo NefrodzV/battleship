69bae056fe20363aed7d3182577ab876bdbb3a3d
Neftaly <neftalyrv23@gmail.com>
2023-09-24 19:17:24 -0400

    Did tests when ships are placed in edges of the board 0 or 9 in either axis

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,8 +1,22 @@
 const Gameboard = require("../Gameboard")
 
-test("Place ship in gameboard", () => {
+test("place ship at the end x is 9", () => {
   const gameboard = Gameboard()
-  gameboard.placeShip(0, 0)
+  gameboard.placeShip(9, 0, gameboard.Orientation.HORIZONTAL)
+  expect(gameboard.getCoordinates()[0][9]).toEqual({ x: 9, y: 0, ship: 0 })
+})
+
+test("Place ship in gameboard in vertical position and the y is 0", () => {
+  const gameboard = Gameboard()
+  gameboard.placeShip(0, 0, gameboard.Orientation.VERTICAL)
+
+  expect(gameboard.getCoordinates()[0][0]).toEqual({ x: 0, y: 0, ship: 0 })
+  expect(gameboard.getCoordinates()[0][1]).toEqual({ x: 0, y: 1, ship: 0 })
+})
+
+test("Place ship in gameboard when the x is 0 and horizontal", () => {
+  const gameboard = Gameboard()
+  gameboard.placeShip(0, 0, gameboard.Orientation.HORIZONTAL)
 
   expect(gameboard.getCoordinates()[0][0]).toEqual({ x: 0, y: 0, ship: 0 })
   expect(gameboard.getCoordinates()[0][1]).toEqual({ x: 1, y: 0, ship: 0 })
