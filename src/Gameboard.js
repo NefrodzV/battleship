e07aa8c68f527005da3c8c3ccc2de1db224513e8
Neftaly <neftalyrv23@gmail.com>
2023-09-27 10:54:51 -0400

    Made the placement of ships without tranposing the array also changed that would be incorrect because of this. Chnaged the name of the observer in mock

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -6,12 +6,13 @@
  * 5 - Report whether  all the ships have been sunked or not
  */
 const Ship = require("./Ship")
-function Gameboard(observer) {
+function Gameboard(errorObserver) {
   const COLS = 10
   const ROWS = 10
 
   const INCORRECT_PLACEMENT_MESSAGE = "Not a valid placement coordinate"
   const SHIP_ALREADY_IN_PLACE_MESSAGE = "Ship already in place"
+
   // Class that has the orientation parameters of  how the ships will be placed
   const Orientation = {
     VERTICAL: "Vertical", // Sets the board array to columns
@@ -21,55 +22,47 @@ function Gameboard(observer) {
   const availableShips = [2, 3, 3, 4, 5]
   const ships = []
 
-  let coordinatesOrientation = Orientation.HORIZONTAL
+  // let coordinatesOrientation = Orientation.HORIZONTAL
   let coordinates = []
 
   // By default the board will be done with Horizontal
   const createCoordinates = (() => {
-    for (let y = 0; y < COLS; y++) {
+    for (let y = 0; y < ROWS; y++) {
       // Setting empty array for each row
       coordinates.push([])
-      for (let x = 0; x < ROWS; x++) {
+      for (let x = 0; x < COLS; x++) {
         coordinates[y].push({ x, y, ship: null })
       }
     }
   })()
 
-  const transponse = (orientation) => {
-    const arrayTransposed = []
-    switch (orientation) {
-      case Orientation.VERTICAL:
-        for (let x = 0; x < COLS; x++) {
-          arrayTransposed.push([])
-          for (let y = 0; y < ROWS; y++) {
-            arrayTransposed[x].push(coordinates[y][x])
-          }
-        }
-        coordinatesOrientation = orientation
-        break
-      case Orientation.HORIZONTAL:
-        for (let y = 0; y < COLS; y++) {
-          arrayTransposed.push([])
-          for (let x = 0; x < ROWS; x++) {
-            arrayTransposed[y].push(coordinates[x][y])
-          }
-        }
-        coordinatesOrientation = orientation
-        break
-
-      default:
-        console.log("Set a correct orientation")
-    }
-
-    // console.log(arrayTransposed)
-    // Deleting current elements and adding the new ones
-    coordinates = arrayTransposed
-
-    // coordinates.length = 0
-    // for (let i = 0; i < arrayTransposed.length; i++) {
-    //   coordinates.push(arrayTransposed[i])
-    // }
-  }
+  //   const transponse = (orientation) => {
+  //     const arrayTransposed = []
+  //     switch (orientation) {
+  //       case Orientation.VERTICAL:
+  //         for (let x = 0; x < COLS; x++) {
+  //           arrayTransposed.push([])
+  //           for (let y = 0; y < ROWS; y++) {
+  //             arrayTransposed[x].push(coordinates[y][x])
+  //           }
+  //         }
+  //         coordinatesOrientation = orientation
+  //         break
+  //       case Orientation.HORIZONTAL:
+  //         for (let y = 0; y < COLS; y++) {
+  //           arrayTransposed.push([])
+  //           for (let x = 0; x < ROWS; x++) {
+  //             arrayTransposed[y].push(coordinates[x][y])
+  //           }
+  //         }
+  //         coordinatesOrientation = orientation
+  //         break
+  //
+  //       default:
+  //         console.log("Set a correct orientation")
+  //     }
+  //     coordinates = arrayTransposed
+  //   }
 
   const areEqual = (shipOrientation, correctOrientation) => {
     return shipOrientation === correctOrientation
@@ -92,7 +85,7 @@ function Gameboard(observer) {
 
       case Orientation.VERTICAL:
         for (let i = 0; i < length; i++) {
-          const shipPointer = coordinates[x][y + i].ship
+          const shipPointer = coordinates[y + i][x].ship
           if (shipPointer !== null) {
             spaceTaken = true
             break
@@ -132,19 +125,20 @@ function Gameboard(observer) {
     Orientation,
 
     placeShip(x, y, shipOrientation) {
-      // TODO: MAKE CHECK IF THE POSITION PLACEMENT POSITION IS VALID OR NOT
-      // USE MOCKS HERE FOR THIS
-      if (!areEqual(shipOrientation, coordinatesOrientation)) {
-        transponse(shipOrientation)
-      }
+      // Checks if the array is in the correct orientation to run the logic
+      // if (!areEqual(shipOrientation, coordinatesOrientation)) {
+      //   transponse(shipOrientation)
+      // }
 
+      // If the placement goes out of bounds stop and notify user
       if (!isValidPlacement(x, y, shipOrientation)) {
-        observer.notify(INCORRECT_PLACEMENT_MESSAGE)
+        errorObserver.notify(INCORRECT_PLACEMENT_MESSAGE)
         return
       }
 
+      // If there is a ship already in place stop and notify user
       if (isShipInPlace(x, y, shipOrientation)) {
-        observer.notify(SHIP_ALREADY_IN_PLACE_MESSAGE)
+        errorObserver.notify(SHIP_ALREADY_IN_PLACE_MESSAGE)
         return
       }
 
@@ -154,16 +148,16 @@ function Gameboard(observer) {
       ships.push(ship)
       const shipPointer = ships.length - 1
 
-      // When Y is always the same but X isnt ROWS
+      // When Y is always the same but X isnt. Sets the ship in the x coordinates
       if (shipOrientation === Orientation.HORIZONTAL) {
         for (let i = 0; i < length; i++) {
           coordinates[y][x + i].ship = shipPointer
         }
       }
-      // When X is always the same but Y isnt COLUMNS
+      // When X is always the same but Y isnt. Sets the ship in the y coordinates
       if (shipOrientation === Orientation.VERTICAL) {
         for (let i = 0; i < length; i++) {
-          coordinates[x][y + i].ship = shipPointer
+          coordinates[y + i][x].ship = shipPointer
         }
       }
     },
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -3,7 +3,7 @@ const Gameboard = require("../Gameboard")
 const subscribe = jest.fn()
 const notify = jest.fn()
 
-const mockObserver = jest.fn(() => {
+const mockErrorObserver = jest.fn(() => {
   return {
     subscribe,
     notify,
@@ -19,8 +19,9 @@ const mockObserver = jest.fn(() => {
  * 5- WE CHECK IF THERE ARE STILL SHIPS STANDING IF NOT WE CAN CONTINUE REGISTERING HITS OR DECLARE THE WINNER
  */
 
+test("Player makes a ")
 test("Place a ship in vertical and horizontal position", () => {
-  const gameboard = Gameboard(mockObserver())
+  const gameboard = Gameboard(mockErrorObserver())
   gameboard.placeShip(0, 1, gameboard.Orientation.VERTICAL)
   gameboard.placeShip(0, 0, gameboard.Orientation.HORIZONTAL)
 
@@ -36,20 +37,20 @@ test("Place a ship in vertical and horizontal position", () => {
 })
 
 test("Check if notify gets called when a ship is already in the board coordinate", () => {
-  const gameboard = Gameboard(mockObserver())
+  const gameboard = Gameboard(mockErrorObserver())
   gameboard.placeShip(0, 0, gameboard.Orientation.HORIZONTAL)
   gameboard.placeShip(1, 0, gameboard.Orientation.HORIZONTAL)
   expect(notify).toBeCalled()
 })
 
 test("place ship at the end x is 9 and observer notify function gets called", () => {
-  const gameboard = Gameboard(mockObserver())
+  const gameboard = Gameboard(mockErrorObserver())
   gameboard.placeShip(9, 0, gameboard.Orientation.HORIZONTAL)
   expect(notify).toBeCalled()
 })
 
 test("Place a ship and checking its equal to the its length in the board", () => {
-  const gameboard = Gameboard(mockObserver())
+  const gameboard = Gameboard(mockErrorObserver())
   gameboard.placeShip(2, 0, gameboard.Orientation.HORIZONTAL)
   expect(gameboard.getCoordinates()[0][4]).toEqual({ x: 4, y: 0, ship: null })
   expect(gameboard.getCoordinates()[0][3]).toEqual({ x: 3, y: 0, ship: 0 })
@@ -58,20 +59,20 @@ test("Place a ship and checking its equal to the its length in the board", () =>
 })
 
 test("Place ship in gameboard in vertical position and the y is 0", () => {
-  const gameboard = Gameboard(mockObserver())
+  const gameboard = Gameboard(mockErrorObserver())
   gameboard.placeShip(0, 0, gameboard.Orientation.VERTICAL)
   expect(gameboard.getCoordinates()[0][0]).toEqual({ x: 0, y: 0, ship: 0 })
-  expect(gameboard.getCoordinates()[0][1]).toEqual({ x: 0, y: 1, ship: 0 })
+  expect(gameboard.getCoordinates()[1][0]).toEqual({ x: 0, y: 1, ship: 0 })
 })
 
 test("Place ship in gameboard when the x is 0 and horizontal", () => {
-  const gameboard = Gameboard(mockObserver())
+  const gameboard = Gameboard(mockErrorObserver())
   gameboard.placeShip(0, 0, gameboard.Orientation.HORIZONTAL)
   expect(gameboard.getCoordinates()[0][0]).toEqual({ x: 0, y: 0, ship: 0 })
   expect(gameboard.getCoordinates()[0][1]).toEqual({ x: 1, y: 0, ship: 0 })
 })
 
 test("Gameboard coordinates are 10 in length", () => {
-  expect(Gameboard(mockObserver()).getCoordinates().length).toBe(10)
-  expect(Gameboard(mockObserver()).getCoordinates()[0].length).toBe(10)
+  expect(Gameboard(mockErrorObserver()).getCoordinates().length).toBe(10)
+  expect(Gameboard(mockErrorObserver()).getCoordinates()[0].length).toBe(10)
 })
