59109c1c3c4a4b251d681239517680be4cc65a2f
Neftaly <neftalyrv23@gmail.com>
2023-10-03 20:31:18 -0400

    removed test for the computer class

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -11,19 +11,3 @@ test("Player with PlayerType.COMPUTER should return the object the computer modu
     computer,
   })
 })
-
-test("Implementing the while logic in here from the computer to see if it runs as expected", () => {
-  const plays = [
-    {
-      x: 0,
-      y: 0,
-    },
-  ]
-
-  const x = 0
-  const y = 0
-
-  const itHas = plays.some((i) => i.x === 0 && i.y === y)
-
-  expect(itHas).toBe(true)
-})
