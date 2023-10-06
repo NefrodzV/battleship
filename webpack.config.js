fabb73cbf367a883a8685b8e456b6b01a3a5a63e
Neftaly <neftalyrv23@gmail.com>
2023-10-06 16:59:47 -0400

    update package for webpack babel

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", { targets: "defaults" }],
+            presets: ["@babel/preset-env"],
           },
         },
       },
