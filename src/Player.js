46cf0069cdcd593bc20d77d48a64a83849f9b9dd
Neftaly <neftalyrv23@gmail.com>
2023-10-06 18:07:10 -0400

    added function to return if its ai or player

--- src/Player.js
+++ src/Player.js
@@ -2,15 +2,22 @@ import { PlayerType } from "./PlayerType"
 import { computer } from "./computer"
 
 export default function Player(name, type) {
+  const isHuman = () => {
+    if (type === PlayerType.COMPUTER) return false
+    return true
+  }
+
   if (type === PlayerType.COMPUTER) {
     return {
       name,
       type,
       computer,
+      isHuman,
     }
   }
   return {
     name,
     type,
+    isHuman,
   }
 }
