9159f9ce0bf605750ffb62ad41fd9267c4e70b3c
Neftaly <neftalyrv23@gmail.com>
2023-10-10 19:28:23 -0400

    Added Notifcations codes for get player id and placing ships on board. Also made function to set the game mode and the player. Furthermore, made the functions related to the placement on ships in the current displaying board in ui

--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,15 @@
+import Player from "./Player"
 // Maybe be able to run functions in the constructor
 export default function Game() {
 
   // What will be display to the user interface
   const Notifications = {
-          MODE_SELECTION: 0
+          MODE_SELECTION: 0,
+          MODE_GET_PLAYER_ID:1,
+          MODE_PLACE_SHIP:2
   }
 
+  // Mode for versus selection
   const GameModes = {
       PLAYER: "Player",
       COMPUTER: "Computer"
@@ -15,29 +19,81 @@ export default function Game() {
   let playerTwo = null
   let isSinglePlayerGame = null
 
-  
-
   const Notifier = {
     // Function to notify the ui
     notify: null
   }
 
-  
-  // Needs to run after the object is initialized
   const init = () => {
     if(playerOne === null && playerTwo === null) {
       Notifier.notify(Notifications.MODE_SELECTION)
-      // But this code is running before the object instance
-      
     }
   }
   
   const setGameMode = (mode) => {
-    console.log(mode)
+    if(mode === GameModes.COMPUTER) {
+      isSinglePlayerGame = true
+      Notifier.notify(Notifications.MODE_GET_PLAYER_ID)
+    }
   }
+  
+  const setPlayer = (name) => {
+    if(isSinglePlayerGame) {
+      playerOne = Player(name, true)
+      playerTwo = Player("AI", false)
+      Notifier.notify(Notifications.MODE_PLACE_SHIP)
+      return;
+    }
+
+    // TODO: Here goes the logic to make a second player if needed for the future update
+  }
+
+
+  //TODO: HANDLE THE LOGIC FOR PLACING SHIPS AND RENDERING THEIR OUTLINES
+  // Get the current player board
+  const getCurrentBoardToDropShips =  () => {
+    if(isSinglePlayerGame) {
+      return playerOne.getBoard()
+    }
+  }
+  // Get the current board to place ships coordinates
+  const getCurrentBoardCoordinates = () => {
+    const board = getCurrentBoardToDropShips()
+    const coordinates = board.getCoordinates()
+
+    return coordinates
+  }
+  // Gets the current player outlines for placing ships
+  const getCurrentBoardOutlines = (x, y) => {
+    const board = getCurrentBoardToDropShips()
+    return board
+  }
+
+  // Changes the current board ship orientation to place ships
+  const changeCurrentBoardShipOrientation = () => {
+      const board = getCurrentBoardToDropShips()
+      return board.updateShipOrientation()
+  }
+
+  const setShipInCurrentBoard = (x,y) => {
+    const board = getCurrentBoardToDropShips()
+    // TODO  CHANGE THE IMPLEMENTATION TO NOFITY USER HERE
+    return board.placeShip(x,y)
+  }
+
+  // TODO Maybe rename and set the logic for the firing of the boards
+  const fireAttackOnCurrentBoard = () => {
+
+  }
+
   
  
   return {
+    getCurrentBoardCoordinates,
+    getCurrentBoardToDropShips,
+    getCurrentBoardOutlines,
+    changeCurrentBoardShipOrientation,
+    setPlayer,
     setGameMode,
     Notifier,
     init,
