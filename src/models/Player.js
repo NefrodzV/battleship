a223b94506a8823c92c7fb9ad6a29d8d047b6d4e
Neftaly <neftalyrv23@gmail.com>
2023-10-12 18:55:24 -0400

    Made ai to have ships placed and add isHuman bool

--- src/models/Player.js
+++ src/models/Player.js
@@ -3,12 +3,20 @@ import Board from "./Board"
 export default function Player(name, isHuman) {
   const board = Board()
 
+  
+
   const getBoard = () => {
     return board
   }
-  if (!isHuman) {
+  if (!isHuman) { 
+  board.placeShip(0,0)
+  board.placeShip(0,1)
+  board.placeShip(0,2)
+  board.placeShip(0,3)
+  board.placeShip(0,4)
     return {
       name,
+      isHuman,
       computer,
       getBoard
     }
@@ -16,6 +24,7 @@ export default function Player(name, isHuman) {
 
   return {
     name,
+    isHuman,
     getBoard,
   }
 }
